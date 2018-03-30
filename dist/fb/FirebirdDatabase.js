7bdcb719bf110dcfb7fd9fbf918449940ed31a75
Andrej Kirejeŭ <gs1994@gmail.com>
2018-03-30 12:52:33 +0300

    these files are changed after npm update command has been executed

--- dist/fb/FirebirdDatabase.js
+++ dist/fb/FirebirdDatabase.js
@@ -1,7 +1,7 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-}
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const ADatabase_1 = require("../ADatabase");
 const FirebirdTransaction_1 = require("./FirebirdTransaction");
